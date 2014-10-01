c77ddfd5d244bba6e38d29e6ab6e37a802d28afa
Shanti Bouchez-Mongardé <shanti-pub.git@mildred.fr>
2014-10-01 14:59:25 +0200

    revert

--- edit-main.js
+++ edit-main.js
@@ -28,15 +28,6 @@ jQuery(function( $ ) {
                 'pOjwAJFkSCSyQrrhRDOYILXFSuNkpjggwtvo86H7YAZ1korkRaEYJlC3WuESxBggJLWHGGFhcIxgBvUHQyUT1GQWwhFxuFKyBPakx'+
                 'NXgceYY9HCDEZTlxA8cOVwUGBAAA7AAAAAAAAAAAA"/ alt="please wait">';
 
-  var centered_spinner = '<div style="position: fixed; top: 50%; left: 50%; margin-left: -8px; margin-top: -8px">' + spinner + '</div>';
-
-  var overlay = $("<div></div>")
-    .css("position", "absolute")
-    .css("left", 0)
-    .css("right", 0)
-    .css("top", 0)
-    .css("bottom", 0);
-
   var topbar = $('<div><h1>Editing document</h1>'+
       '<button class="save">Save</button>'+
       '<button class="close">Close Without Saving</button>'+
@@ -47,8 +38,6 @@ jQuery(function( $ ) {
     .css('padding-right', '1em')
     .css('padding-bottom', '16px');
   topbar.children().filter(":first").css("margin-top", 0);
-
-  var iframe;
 
   var toolbar = $('<div></div>')
     .css('position', 'absolute')
@@ -115,145 +104,29 @@ jQuery(function( $ ) {
   var editing;
 
   function start_edit(){
-    show_overlay();
-    var iframe_doc;
-    $.get(window.location.href, function(data){
-      iframe = $("<iframe></iframe>")
-        .css("border", "none")
-        .css("width", "100%")
-        .css("overflow", "none")
-        .attr("scrolling", "no")
-        .attr("src", window.location.href)
-        .height(document.documentElement.scrollHeight - topbar.height())
-        .replaceAll(iframe);
-
-      var win = iframe[0].contentWindow;
-      win.location = location;
-      iframe_doc = iframe.contents()[0];
-      iframe_doc.write(data);
-      setTimeout(resizeIframe, 1000);
-
-      var loaded = false;
-      editing = $();
-
-      var observer = new MutationObserver(function(mutations) {
-        if(editing.length == 0) {
-          editing = $(iframe_doc.body).children().filter(":visible").filter(":not(.aloha)");
-          if(editing.length != 0) {
-
-            /*
-            $.get("jquery-2.1.1.min.js", function(data){
-              $('<script type="text/javascript"></script>')
-                .text(data)
-                .appendTo(iframe_doc.head);
-            }, "text");
-            $.get("alohaeditor-1.1.0/aloha/css/aloha.css", function(data){
-              $('<style type="text/css"></style>')
-                .text(data)
-                .appendTo(iframe_doc.head);
-            }, "text");
-            $.get("alohaeditor-1.1.0/aloha/lib/require.js", function(data){
-              $('<script type="text/javascript"></script>')
-                .text(data)
-                .appendTo(iframe_doc.head);
-            }, "text");
-            $.get("alohaeditor-1.1.0/aloha/lib/aloha.js", function(data){
-              $('<script type="text/javascript"></script>')
-                .attr("data-aloha-plugins",  "common/ui,common/format,common/highlighteditables,common/link")
-                .text(data)
-                .appendTo(iframe_doc.head);
-            }, "text");
-            */
-
-            var base = location.href.replace(/\/[^\/]*$/, "/");
-            $(iframe_doc.createElement("script"))
-              .attr("type", "text/javascript")
-              .text("Aloha = {}; Aloha.settings = { sidebar: { disabled: true } };")
-              .appendTo(iframe_doc.head);
-            $(iframe_doc.createElement("link"))
-              .attr("rel", "stylesheet")
-              .attr("type", "text/css")
-              .attr("href",  base + "alohaeditor-1.1.0/aloha/css/aloha.css")
-              .appendTo(iframe_doc.head);
-            /*
-            $(iframe_doc.createElement("script"))
-              .attr("type", "text/javascript")
-              .attr("src",  base + "jquery-2.1.1.min.js")
-              .appendTo(iframe_doc.head);
-            $(iframe_doc.createElement("script"))
-              .attr("type", "text/javascript")
-              .attr("src",  base + "alohaeditor-1.1.0/aloha/lib/require.js")
-              .appendTo(iframe_doc.head);
-            $(iframe_doc.createElement("script"))
-              .attr("type", "text/javascript")
-              .attr("src",  base + "alohaeditor-1.1.0/aloha/lib/aloha.js")
-              .attr("data-aloha-plugins",  "common/ui,common/format,common/highlighteditables,common/link")
-              .appendTo(iframe_doc.head);
-            */
-            /*
-            $(iframe_doc.createElement("script"))
-              .attr("type", "text/javascript")
-              .attr("src",  base + "alohaeditor-1.1.0/aloha/lib/aloha-full.js")
-              .attr("data-aloha-plugins",  "common/ui,common/format,common/highlighteditables,common/link")
-              .appendTo(iframe_doc.head);
-            */
-            $(iframe_doc.createElement("script"))
-              .attr("type", "text/javascript")
-              .text('"$(iframe_doc.body).children().filter(":visible").filter(":not(.aloha)").each(function(){ win.Aloha.jQuery(this).aloha(); })')
-              .appendTo(iframe_doc.head);
-
-            /*editing.each(function(){
-              win.Aloha.jQuery(this).aloha();
-            });*/
-          }
-        }
-        if(!loaded && $(iframe_doc.body).height() != 0) {
-          loaded = true;
-          resizeIframe();
-        }
-      });
-      observer.observe(iframe_doc, {attributes: true, childList: true, characterData: true, subtree: true});
-
-      show_edit_header(overlay);
-    }, "text");
-
-    function resizeIframe(){
-      iframe.attr("scrolling", "auto")
-      iframe.height($(iframe_doc.body).height()+16);
-    }
+    toolbar.detach();
+    editing = $('body').children().filter(":visible").filter(":not(.aloha)");
+    editing.each(function(){
+      Aloha.jQuery(this).aloha();
+    });
+    show_edit_header();
+    $(":not([data-generated])").attr("data-generated", "true")
   }
 
   function stop_edit(){
-    /*
+    $(":not([data-generated])").attr("data-generated", "false")
     editing.each(function(){
       Aloha.jQuery(this).mahalo().removeClass('aloha-editable-highlight');
     });
-    */
   }
 
-  function show_overlay(){
-    $("body > *").hide();
-    overlay
-      .show()
-      .prependTo($('body'));
-    iframe = $('<iframe sandbox=""></iframe>')
-      .prependTo(overlay)
-      .css("border", "none")
-      .css("width", "100%")
-      .css("height", "100%")
-      .attr("srcdoc", centered_spinner);
-  }
-
-  function show_edit_header(container){
-    topbar.prependTo(container || $('body')).hide().slideDown();
+  function show_edit_header(){
+    topbar.prependTo($('body')).hide().slideDown();
   }
 
   function hide_edit_header(){
     topbar.slideUp(function(){
       topbar.detach();
-      iframe.detach();
-      overlay.detach();
-      $("body > *").show();
       toolbar.prependTo($('body'));
     });
   }
